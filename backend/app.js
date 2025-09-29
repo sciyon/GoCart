e05298ec4d0782df241e1aefa416d1dbc8884780
Erwin Antepuesto <antepuesto.erwin@gmail.com>
2025-09-29 17:30:41 +0800

    nodemon setup for prod and dev

--- backend/app.js
+++ backend/app.js
@@ -5,5 +5,5 @@ const app = express();
 dotenv.config({ path: './config/config.env' });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on PORT: ${process.env.PORT}`);
+  console.log(`Server is running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
