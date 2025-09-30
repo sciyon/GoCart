87754dedf08fa825a3eb8abbbaa4da8b9004737a
Erwin Antepuesto <antepuesto.erwin@gmail.com>
2025-10-01 04:30:03 +0800

    get product info

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,21 +1,51 @@
 import Product from "../models/product.js";
 
+/*
+    Get all products => /api/v1/products
+*/
 export const getProducts = async(req, res) => {
+    try{
+        const products = await Product.find();
 
-    const products = await Product.find();
-
-    res.status(200).json({ 
-        products
-    });
+        res.status(200).json({ 
+            success: true,
+            products
+        });
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
 }
 
 /*
-    Create new product
+    Create new product=> /api/v1/admin/products
 */
 export const newProduct = async(req, res) => {
     const product =  await Product.create(req.body);
 
     res.status(200).json({
+        success: true,
         product
     });
 }
+
+/*
+    Get single product => /api/v1/product/:id
+*/
+export const getProductDetails = async(req, res) => {
+    const product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    });    
+}         
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,10 @@
-import { getProducts, newProduct } from "../controllers/productController.js";
+import { getProducts, newProduct, getProductDetails } from "../controllers/productController.js";
 import express from "express";
 
 const router = express.Router();
 
 router.route('/products').get(getProducts);
 router.route('/admin/products').post(newProduct);
+router.route('/product/:id').get(getProductDetails);
 
 export default router; 
